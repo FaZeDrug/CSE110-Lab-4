67a16bc550e87598d229d854f1d816985a91ea75
mariawaseem <97696497+mariawaseem@users.noreply.github.com>
2024-11-04 20:54:06 -0800

    Exercise 2 Done

--- client/src/components/Budget/Budget.tsx
+++ client/src/components/Budget/Budget.tsx
@@ -1,7 +1,27 @@
+import React, { useEffect, useContext } from "react";
+import { AppContext } from "../../context/AppContext";
+import { fetchBudget } from "../../utils/budget-utils";
+
 const Budget = () => {
+  const { budget, setBudget } = useContext(AppContext);
+
+  useEffect(() => {
+    loadBudget();
+  }, []);
+
+  // Function to load expenses and handle errors
+  const loadBudget = async () => {
+    try {
+    const budget = await fetchBudget();
+    setBudget(budget);
+    } catch (err: any) {
+    console.log(err.message);
+    }
+  };
+
   return (
     <div className="alert alert-secondary p-3 d-flex align-items-center justify-content-between">
-      <div>Budget: $1000</div>
+      <div>Budget: ${ budget }</div>
     </div>
   );
 };
